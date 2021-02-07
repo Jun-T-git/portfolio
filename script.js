5443848b0101de5a84f8c05257398b9e2b3996a4
Jun-T-git <64676197+Jun-T-git@users.noreply.github.com>
2021-02-07 19:26:48 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+  Particles.init({
+    selector: '.background',
+    sizeVariations: 10,
+    color: ['#EEEEEE', '#AAAABB', '#777788'],
+    connectParticles: true,
+  },
+  );
+};
